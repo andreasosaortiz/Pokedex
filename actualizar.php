02c2274f9679b096916cc949e1222b92deb97190
Ailen Pereira Vilches <ailenpereiravilches@gmail.com>
2023-09-25 14:20:57 -0300

    modifico método para actualizar

--- actualizar.php
+++ actualizar.php
@@ -1,29 +1,43 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $nombre = $_POST["nombre"];
     $descripcion = $_POST["descripcion"];
     $numero_id = $_POST["numero_identificador"];
 
-    if (isset($_GET["id"])) {
-        $id_pokemon_a_actualizar = $_GET["id"];
+    if (!empty($nombre) || !empty($descripcion) || !empty($numero_id)) {
 
-        require_once('conexion.php');
+        if (isset($_GET["id"])) {
+            $id_pokemon_a_actualizar = $_GET["id"];
 
-    $id_pokemon_a_eliminar = $_GET["id"];
+            require_once('conexion.php');
 
-    $sql = "UPDATE pokemon SET nombre = '$nombre', descripcion = '$descripcion' WHERE id = $id_pokemon_a_actualizar";
+            $actualizar_campos = [];
+            if (!empty($nombre)) {
+                $actualizar_campos[] = "nombre = '$nombre'";
+            }
+            if (!empty($descripcion)) {
+                $actualizar_campos[] = "descripcion = '$descripcion'";
+            }
+            if (!empty($numero_id)) {
+                $actualizar_campos[] = "numero_identificador = '$numero_id'";
+            }
 
-        if ($conexion->query($sql) === TRUE) {
-        echo "Pokemon actualizado con éxito";
-        header("Location: index.php");
-        exit;
-        } else {
-        echo "Error al actualizar pokemon: " . $conexion->error;
+            $update_sql = "UPDATE pokemon SET " . implode(', ', $actualizar_campos) . " WHERE id = $id_pokemon_a_actualizar";
+
+            if ($conexion->query($update_sql) === TRUE) {
+                echo "Pokemon actualizado con éxito";
+                header("Location: index.php");
+                exit;
+            } else {
+                echo "Error al actualizar pokemon: " . $conexion->error;
+            }
+
+            $conexion->close();
         }
-
-    $conexion->close();
-}
+    } else {
+        echo "No se pudo actualizar.";
+    }
 }
 ?>
+
